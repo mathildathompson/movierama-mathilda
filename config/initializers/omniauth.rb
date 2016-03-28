7ccfdd57a86c7a2168d37fcd5a6a4b3bcacbd389
Mathilda Thompson <mathilda.thompson@digital.cabinet-office.gov.uk>
2016-03-28 17:55:32 +0100

    Add email scope to github omniauth login to enable storing email attribute in user model

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,6 @@ require 'omniauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :github,
-    ENV.fetch('GITHUB_OAUTH_CLIENT_ID'),
-    ENV.fetch('GITHUB_OAUTH_SECRET')
+  provider :github, ENV.fetch('GITHUB_OAUTH_CLIENT_ID'), ENV.fetch('GITHUB_OAUTH_SECRET'), scope: "user:email"
 end
 
