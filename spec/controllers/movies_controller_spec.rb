cd01872e49abccc57739601d23389cd502276a80
Mathilda Thompson <mathilda.thompson@digital.cabinet-office.gov.uk>
2016-03-29 13:46:40 +0100

    Complete pending movies_controller test

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,25 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController, :type => :controller do
+  let(:token) { SecureRandom.hex }
+  let(:user) { User.create(token: token) }
+
+  before do
+    session[:token] = token
+    Ability.new(user)
+  end
 
   describe "GET index" do
-    xit "returns http success" do
+    it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET new" do
-    xit "returns http success" do
+    it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "POST create" do
-    xit "returns http success" do
-      post :create
-      expect(response).to have_http_status(:success)
+    it "returns http redirect" do
+      post :create, {title: 'test', description: 'test description', date: '1988-11-11'}
+      expect(response).to have_http_status(:redirect)
+      expect(Movie.all.count.to_i).to eq(1)
     end
   end
 
