fa495dbcf3bee5a80f7266685c9b6667dedc2288
Mathilda Thompson <mathilda.thompson@digital.cabinet-office.gov.uk>
2016-03-29 13:47:54 +0100

    Complete pending votes controller test

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,18 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe VotesController, :type => :controller do
+  let(:token) { SecureRandom.hex }
+  let(:user) { User.create(token: token) }
+  let(:movie) { Movie.create }
 
-  describe "GET create" do
-    xit "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  before do
+    session[:token] = token
+    Ability.new(user)
+  end
+
+  describe "POST create" do
+    it "returns http redirect" do
+      expect(Movie[movie.id].liker_count.to_i).to eq(0)
+      post :create, movie_id: movie.id, t: 'like'
+      expect(response).to have_http_status(:redirect)
+      expect(Movie[movie.id].liker_count.to_i).to eq(1)
     end
   end
 
-  describe "GET destroy" do
-    xit "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+  describe "DELETE destroy" do
+    before do
+      post :create, movie_id: movie.id, t: 'like'
+    end
+    it "returns http redirect" do
+      delete :destroy, movie_id: movie.id
+      expect(response).to have_http_status(:redirect)
+      expect(Movie[movie.id].liker_count.to_i).to be(0)
     end
   end
 
